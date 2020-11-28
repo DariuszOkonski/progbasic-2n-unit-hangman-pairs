8d783929f539510218767afc298ba1ac1ee262ee
Dariusz Okonski <darek200180@gmail.com>
2020-11-28 19:08:06 +0000

    add draw_hangman fuctions

--- picture.py
+++ picture.py
@@ -3,57 +3,66 @@ import os
 
 picture7 = """
     ||=====|
-    |/     0
+    |/     O
     ||   -{ }-
     ||    / \\
-    || GAME OVER  
+    || GAME OVER
+==================     
 """
 picture6 = """
     ||=====|
-    |/     0
+    |/     O
     ||   -{ }-
     ||    / 
     ||    
+==================
 """
 picture5 = """
     ||=====|
-    |/     0
+    |/     O
     ||   -{ }-
     ||    
     ||    
+==================
 """
 picture4 = """
     ||=====|
-    |/     0
+    |/     O
     ||   -{ }
     ||    
     ||    
+==================
 """
 picture3 = """
     ||=====|
-    |/     0
+    |/     O
     ||    { }
     ||  
     ||    
+==================
 """
 picture2 = """
     ||=====|
-    |/     0
+    |/     O
     ||   
     ||   
     ||    
+==================
 """
 picture1 = """
     ||=====|
     |/    
     ||   
     ||    
-    ||    
+    ||  
+==================  
 """
+
+hangman_list = [picture1, picture2, picture3, picture4, picture5, picture6, picture7]
 
 def console_clear():
     os.system('cls')
-hangman_list = [picture1, picture2, picture3, picture4, picture5, picture6, picture7]
+    
 
 def draw_single_hangman(index):
     console_clear()
@@ -61,7 +70,6 @@ def draw_single_hangman(index):
 
 
 def draw_hangman(hangman_list):
-
     for item in hangman_list:
         console_clear()
         print(item)
